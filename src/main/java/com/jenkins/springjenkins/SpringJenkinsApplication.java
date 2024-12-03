0c68e39b450c1ca764241b6006f97343536e50c5
haitam-elgharras <elgharras.haitam@gmail.com>
2024-12-03 19:38:04 +0100

    basic project setup to test jenkins

---
+++ src/main/java/com/jenkins/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.jenkins.springjenkins;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+//    add a logger
+    private static final Logger LOG = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    // add a post-construct method
+    @PostConstruct
+    public void init(){
+        LOG.info("Application started...");
+    }
+
+    public static void main(String[] args) {
+        LOG.info("Application executed...");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.jenkins.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+    // add a logger
+    private static final Logger LOG = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        LOG.info("Test case executing...");
+        assertTrue(true);
+    }
+
+}
